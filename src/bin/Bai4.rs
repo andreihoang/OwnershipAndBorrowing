68838b00997c5fe8e65923cde5488ad9f5c3a6a8
Dang Huy Hoang <dang_huy.hoang@student.unsw.edu.au>
2022-10-23 18:14:21 +0700

    first

---
+++ src/bin/Bai4.rs
@@ -0,0 +1,22 @@
+// ====================================
+// BÀI 4:
+// Yêu cầu: Không cần quan tâm tới logic
+// sửa vấn đề liên quan tới ownership
+
+// =====================================
+
+fn main() {
+    let mut v = vec![1, 2, 3];
+
+    go(&mut v);
+
+    // still need v here, so I can't pass ownership to the "go' method above
+    println!("{}", v.len())
+}
+
+fn go(v: &mut Vec<i32>) {
+    for i in v.iter_mut() {
+        println!("{}", *i);
+    }
+    v.push(4);
+}
